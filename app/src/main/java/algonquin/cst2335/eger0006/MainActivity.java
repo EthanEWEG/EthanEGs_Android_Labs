382b81456ad576ba5de1be7d33dc20935b8586b5
Ethan EG <ethaneweg@gmail.com>
2023-05-19 14:07:27 -0400

    Commit 6: Created editText field and OnClick button listener to change the helloWorld text

--- app/src/main/java/algonquin/cst2335/eger0006/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/eger0006/MainActivity.java
@@ -2,7 +2,12 @@ package algonquin.cst2335.eger0006;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaring based on id's
+        TextView theText = findViewById(R.id.textview);
+        Button myButton =findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+
+        //button click listener
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this is run when you click the button
+                String editString = myedit.getText().toString();
+                theText.setText("Your edit text has : "+ editString);
+            }
+        });
     }
 }

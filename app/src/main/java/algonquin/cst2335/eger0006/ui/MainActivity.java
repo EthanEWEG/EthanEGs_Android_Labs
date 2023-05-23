a285bb55e00cbd8639064e8d1bb827266659304c
Ethan EG <ethaneweg@gmail.com>
2023-05-23 14:14:12 -0400

    Commit 9: Added MainViewModel.java class to make the editString variable a MutableLiveData type. Changed MainActivity to include these changes

--- app/src/main/java/algonquin/cst2335/eger0006/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/eger0006/ui/MainActivity.java
@@ -1,6 +1,7 @@
-package algonquin.cst2335.eger0006;
+package algonquin.cst2335.eger0006.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,28 +10,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
+import algonquin.cst2335.eger0006.data.MainViewModel;
 import algonquin.cst2335.eger0006.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding variableBinding;
+    private MainViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        //Declaring based on id's
-        TextView theText = variableBinding.textview;
-        Button myButton =variableBinding.mybutton;
-        EditText myedit = variableBinding.myedittext;
 
         //button click listener
-        myButton.setOnClickListener( vw -> {
+        variableBinding.mybutton.setOnClickListener(click ->
+        {
             //this is run when you click the button
-            String editString = myedit.getText().toString();
-            theText.setText("Your edit text has : "+ editString);
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+            model.editString.observe(this, s -> {
+                variableBinding.textview.setText("Your edit text has : "+ s);
+            });
         });
     }
 }

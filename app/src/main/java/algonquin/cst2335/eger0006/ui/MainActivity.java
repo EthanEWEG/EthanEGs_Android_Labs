312a273c9990080021296664b1e9c2c634232c22
Ethan EG <ethaneweg@gmail.com>
2023-05-23 14:51:21 -0400

    Commit 10: Added Compound buttons and a toast message for btn status.

--- app/src/main/java/algonquin/cst2335/eger0006/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/eger0006/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import algonquin.cst2335.eger0006.data.MainViewModel;
 import algonquin.cst2335.eger0006.databinding.ActivityMainBinding;
@@ -37,5 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 variableBinding.textview.setText("Your edit text has : "+ s);
             });
         });
+
+        model.coffeeSelect.observe(this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+                Toast toast = Toast.makeText(this, "The value is now: "+ variableBinding.switch1.isChecked(),Toast.LENGTH_SHORT);
+                toast.show();
+        });
+
+        variableBinding.checkBox.setOnCheckedChangeListener((checkBox, isChecked)-> {
+            // Update the mutableLiveData object in the MainViewModel
+            model.coffeeSelect.postValue(isChecked);
+        });
+        variableBinding.radioButton.setOnCheckedChangeListener((radioButton, isChecked)-> {
+            // Update the mutableLiveData object in the MainViewModel
+            model.coffeeSelect.postValue(isChecked);
+        });
+        variableBinding.switch1.setOnCheckedChangeListener((switch1, isChecked)-> {
+            // Update the mutableLiveData object in the MainViewModel
+            model.coffeeSelect.postValue(isChecked);
+        });
+
     }
 }
